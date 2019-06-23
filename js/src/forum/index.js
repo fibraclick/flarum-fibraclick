bdad013f2767b972c9cdbfed95b0dfb4763fab7e
Matteo Contrini <matteo@contrini.it>
2019-06-23 22:09:13 +0200

    Add keywords highlighting

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,5 +1,37 @@
 import app from 'flarum/app';
+import { extend } from 'flarum/extend';
+import CommentPost from 'flarum/components/CommentPost';
+
+const mappings = {
+    vula: 'https://fibra.click/vula-slu-nga/',
+    slu: 'https://fibra.click/vula-slu-nga/',
+    nga: 'https://fibra.click/vula-slu-nga/',
+    bul: 'https://fibra.click/piano-bul/',
+    'piano bul': 'https://fibra.click/piano-bul/',
+    vectoring: 'https://fibra.click/vectoring/',
+    vdsl: 'https://fibra.click/vdsl/',
+    gpon: 'https://fibra.click/gpon/',
+    'aree bianche': 'https://fibra.click/aree-nere-grigie-bianche/',
+    'aree nere': 'https://fibra.click/aree-nere-grigie-bianche/',
+    'aree grigie': 'https://fibra.click/aree-nere-grigie-bianche/'
+};
+
+// https://stackoverflow.com/a/18622606/1633924
+const regex = new RegExp('\\b(' + Object.keys(mappings).join('|') + ')\\b(?![^<]*>|[^<>]*</[^p])', 'gi');
 
 app.initializers.add('botfactoryit/fibraclick', () => {
-  
+    extend(CommentPost.prototype, 'init', function (post) {
+        this.props.post.data.attributes.contentHtml =
+            this.props.post.data.attributes.contentHtml
+                .replace(regex, (match) => {
+                    let link = mappings[match.toLowerCase()];
+
+                    if (link) {
+                        return `<a class="wiki" target="_blank" href="${link}">${match}</a>`;
+                    }
+                    else {
+                        return match;
+                    }
+            });
+    });
 });
