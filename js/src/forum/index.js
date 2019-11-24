baae0dec2c7ea853836e3a9722732dc34cd550a7
Matteo Contrini <matteo@contrini.it>
2019-11-24 21:55:37 +0100

    Readd WelcomeHero

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,12 +2,15 @@ import app from 'flarum/app';
 import { extend, override } from 'flarum/extend';
 import CommentPost from 'flarum/components/CommentPost';
 import IndexPage from 'flarum/components/IndexPage';
+import WelcomeHero from 'flarum/components/WelcomeHero';
 import QuickLinksComponent from './QuickLinksComponent';
 import replaceKeywords from './replaceKeywords';
 import extendSidebar from './extendSidebar';
 
 app.initializers.add('botfactoryit/fibraclick', () => {
     extend(CommentPost.prototype, 'init', replaceKeywords);
-    override(IndexPage.prototype, 'hero', () => QuickLinksComponent.component());
+    override(IndexPage.prototype, 'hero', () =>
+        [ WelcomeHero.component(), QuickLinksComponent.component() ]
+    );
     extend(IndexPage.prototype, 'navItems', extendSidebar);
 });
