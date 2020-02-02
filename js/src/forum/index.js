9c8e6253eacefc7f644cc6e01b8ce27b8d372165
Matteo Contrini <matteo@contrini.it>
2020-02-02 16:41:29 +0100

    Log isLoggedIn dimension

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -13,4 +13,14 @@ app.initializers.add('botfactoryit/fibraclick', () => {
         [ WelcomeHero.component(), QuickLinksComponent.component() ]
     );
     extend(IndexPage.prototype, 'navItems', extendSidebar);
+
+    extend(app, 'mount', function (vdom) {
+        if (app.data.googleTrackingCode && gtag) {
+            gtag('config', app.data.googleTrackingCode, {
+                'custom_map': {'dimension1': 'isLoggedIn'}
+            });
+
+            gtag('event', 'isLoggedInEvent', {'isLoggedIn': !!app.session.user });
+        }
+    });
 });
