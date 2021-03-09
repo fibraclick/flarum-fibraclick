68fde87c4ae4221f90e91b231b55ccc8a5733bb6
Matteo Contrini <matteo@contrini.it>
2021-03-09 15:34:32 +0100

    Improve header items logic

--- js/src/forum/addPostHeaderItems.js
+++ js/src/forum/addPostHeaderItems.js
@@ -1,23 +1,24 @@
 export default function (items) {
-    if (this.attrs.post.number() !== 1 && this.attrs.post.discussion().user() === this.attrs.post.user()) {
+    const postUser = this.attrs.post.user();
+
+    if (!postUser) {
+        return;
+    }
+
+    if (this.attrs.post.number() !== 1 && this.attrs.post.discussion().user() === postUser) {
         items.add('author-flair', m('span.item-user-label.author', 'Autore'), 99);
     }
 
     if (app.forum.attribute('fibraclick.flair.show') === '1') {
-        const user = this.attrs.post.user();
+        const flairName = postUser.flairName();
+        const flairColor = postUser.flairColor();
 
-        if (user) {
-            const flairName = user.flairName();
-
-            if (flairName) {
-                const flairColor = this.attrs.post.user().flairColor();
-
-                items.add(
-                    'flair',
-                    m('span.item-user-label.flair', {style: {background: flairColor}}, flairName),
-                    99
-                );
-            }
+        if (flairName && flairColor) {
+            items.add(
+                'flair',
+                m('span.item-user-label.flair', {style: {background: flairColor}}, flairName),
+                99
+            );
         }
     }
 }
