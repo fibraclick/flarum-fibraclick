c0a9e8ddad99919b5d9979a0b40dc6db698f536b
Matteo Contrini <matteo@contrini.it>
2023-02-02 18:36:43 +0100

    Delete avatar when clearing PII

--- src/Controllers/DeleteUserPIIController.php
+++ src/Controllers/DeleteUserPIIController.php
@@ -2,8 +2,10 @@
 
 namespace FibraClick\Controllers;
 
+use Flarum\Bus\Dispatcher;
 use Flarum\Http\RequestUtil;
 use Flarum\Post\Post;
+use Flarum\User\Command\DeleteAvatar;
 use Flarum\User\User;
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Arr;
@@ -15,10 +17,12 @@ use Psr\Http\Server\RequestHandlerInterface;
 class DeleteUserPIIController implements RequestHandlerInterface
 {
     private ConnectionInterface $db;
+    private Dispatcher $bus;
 
-    public function __construct(ConnectionInterface $db)
+    public function __construct(ConnectionInterface $db, Dispatcher $bus)
     {
         $this->db = $db;
+        $this->bus = $bus;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
@@ -36,6 +40,10 @@ class DeleteUserPIIController implements RequestHandlerInterface
         $this->db->table('kilowhat_audit_log')
             ->where('actor_id', $userId)
             ->delete();
+
+        $this->bus->dispatch(
+            new DeleteAvatar($userId, $actor)
+        );
 
         return (new Response())->withStatus(204);
     }
